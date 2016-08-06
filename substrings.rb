d79ce306691a2a2da9d23c9e301da3c6070662b1
rhys <rbrooker105@gmail.com>
2016-08-06 10:08:52 +1000

    Project completed. Final commit

--- substrings.rb
+++ substrings.rb
@@ -1,14 +1,23 @@
-#string.split(' ')
-#non_letters.include?(new_string[-1][-1])
-#non_letters = "?!.<>,"
-def substrings word, diction
-  diction.each do |x| 
-	puts x if word.include?(x)
+def substrings words, diction
+  hash = {}
+  words = words.downcase.scan(/[\w']+/) #split string into array of words
+  words.each do |strings| #Check each word out of the array
+    diction.each do |x| 
+	  if strings.include?(x)
+	    if hash.has_key?(x) #If it already has key add one to the value
+	  	  hash[x] +=1
+		else
+		  hash[x] = 1 #else create key with value 1
+	 	end
+      end
+    end
   end
+  hash
 end
 
+dictionary = gets.chomp
+dictionary = dictionary.downcase.scan(/[\w']+/)
+puts "input words to be searched"
+user_input =gets.downcase.chomp
+puts substrings(user_input,dictionary)
 
-
-
- dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("below", dictionary)
